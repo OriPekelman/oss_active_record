c2df8446f999f07bad8046be7ad62752942ffa19
Ori Pekelman <ori@pekelman.com>
2013-07-11 17:55:02 +0200

    fuller skeleton

--- lib/oss_active_record/searchable.rb
+++ lib/oss_active_record/searchable.rb
@@ -7,13 +7,42 @@ module OssActiveRecord
     end
  
     module ClassMethods
-      def searchable(options = {})
+      @@_fields={:integer=>[],:text=>[],:string=>[],:time=>[]}
+      def _fields
+        @@_fields
+      end
+      
+      def searchable(options = {}, &block)
         #integer text string time
-        binding.pry
+        yield
+        unless options[:auto_index] == false
+          before_save :do_something_before
+          after_save :index
+        end
+      end
+
+      def integer(field_name)
+        @@_fields[:integer]<<field_name
+      end
+      def text(field_name)
+        @@_fields[:text]<<field_name
+      end
+      def string(field_name)
+        @@_fields[:string]<<field_name
+      end
+      def time(field_name)
+        @@_fields[:time]<<field_name
       end
     end
- 
+    
+
+    def search(*args, &block)
+    end
+    
     module LocalInstanceMethods
+      #      def index(self)
+        # here we index the record!
+      #end
     end
 
   end
